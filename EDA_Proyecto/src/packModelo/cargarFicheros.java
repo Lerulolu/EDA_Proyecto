ad35195d1bbfaf2c561a1f620fa845d81ad1ddcd
Lerulolu <46162886+Lerulolu@users.noreply.github.com>
2019-09-17 19:20:13 +0200

    Cargar pelis (creo ke mal)

--- EDA_Proyecto/src/packModelo/cargarFicheros.java
+++ EDA_Proyecto/src/packModelo/cargarFicheros.java
@@ -25,7 +25,15 @@ public class cargarFicheros {
 		try {
 			
 			BufferedReader bf = new BufferedReader(new FileReader("src/packDatos/FilmsActors20162017.txt"));
-			
+			String line = "";
+			line = bf.readLine();
+			while (line != null) {
+				if(line.trim().length() == 0)
+					continue;
+				System.out.println(line);
+				line = bf.readLine();
+			}
+			bf.close();
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
---
+++ EDA_Proyecto/src/packPruebas/main.java
@@ -0,0 +1,14 @@
+package packPruebas;
+
+import packModelo.cargarFicheros;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("EMPIEZA EL PROGRAMA");
+		cargarFicheros.getCargarFicheros().cargarFichero();
+		System.out.println("ACABADO");
+
+	}
+
+}
